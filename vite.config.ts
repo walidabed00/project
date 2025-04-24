adf5991ef2264ccf9336597f9cd91d848eb0b9cf
Walid Abed <walidabed005@gmail.com>
2025-04-24 11:13:21 +0300

    fix: set base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/REPO_NAME/' : '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+}));
